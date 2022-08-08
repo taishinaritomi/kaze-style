cb7c486418fe393d60e5549e689b5ca3f945e131
taishinaritomi <taishinaritomi@gmail.com>
2022-08-08 19:39:19 +0900

    update playground next.config

--- playground/next/next.config.mjs
+++ playground/next/next.config.mjs
@@ -1,15 +1,24 @@
 import { KazePlugin } from '@kaze-style/webpack-loader';
-import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import * as browserslist from 'browserslist';
+import { lazyPostCSS } from 'next/dist/build/webpack/config/blocks/css/index.js';
+import { getGlobalCssLoader } from 'next/dist/build/webpack/config/blocks/css/loaders/index.js';
+
+function getSupportedBrowsers(dir, isDevelopment) {
+  let browsers
+  try {
+    browsers = browserslist.loadConfig({
+      path: dir,
+      env: isDevelopment ? 'development' : 'production',
+    });
+  } catch { }
+  return browsers;
+}
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  webpack(config) {
-    config.optimization.splitChunks = config.optimization.splitChunks || {
-      cacheGroups: {},
-    };
-
+  webpack(config,{isServer,dir, dev}) {
     config.module.rules.unshift(
       {
         test: /\.(js|jsx|ts|tsx)$/,
@@ -20,23 +29,38 @@ const nextConfig = {
             options: {},
           },
         ],
-      },
-      {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
-      },
+      }
     );
 
-    config.module.rules.unshift();
+    const cssRules = config.module.rules.find(
+      (rule) =>
+        Array.isArray(rule.oneOf) &&
+        rule.oneOf.some(
+          ({ test }) =>
+            typeof test === 'object' &&
+            typeof test.test === 'function' &&
+            test.test('filename.css'),
+        ),
+    ).oneOf;
 
-    config.plugins.push(
-      new MiniCssExtractPlugin({
-        filename: 'static/chunks/[chunkhash].css',
-        chunkFilename: 'static/chunks/[chunkhash].css',
-        ignoreOrder: true,
-      }),
-      new KazePlugin(),
-    );
+    cssRules.unshift({
+      test: /kaze\.css$/i,
+      sideEffects: true,
+      use: getGlobalCssLoader(
+        {
+          assetPrefix: config.assetPrefix,
+          isClient: !isServer,
+          isServer,
+          isDevelopment: dev,
+          future: {},
+          experimental: {},
+        },
+        () => lazyPostCSS(dir, getSupportedBrowsers(dir, dev),undefined),
+        [],
+      ),
+    });
+
+    config.plugins.push(new KazePlugin());
 
     return config;
   },
