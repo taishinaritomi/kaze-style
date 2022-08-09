ad3a2cf031e5601ad53d783901721eca96f4cef0
taishinaritomi <taishinaritomi@gmail.com>
2022-08-09 19:05:25 +0900

    update playground next

--- playground/next/next.config.mjs
+++ playground/next/next.config.mjs
@@ -1,69 +1,9 @@
-import { KazePlugin } from '@kaze-style/webpack-loader';
-import * as browserslist from 'browserslist';
-import { lazyPostCSS } from 'next/dist/build/webpack/config/blocks/css/index.js';
-import { getGlobalCssLoader } from 'next/dist/build/webpack/config/blocks/css/loaders/index.js';
-
-function getSupportedBrowsers(dir, isDevelopment) {
-  let browsers
-  try {
-    browsers = browserslist.loadConfig({
-      path: dir,
-      env: isDevelopment ? 'development' : 'production',
-    });
-  } catch { }
-  return browsers;
-}
+import { withKazeStyle } from '@kaze-style/next-plugin';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  webpack(config,{isServer,dir, dev}) {
-    config.module.rules.unshift(
-      {
-        test: /\.(js|jsx|ts|tsx)$/,
-        exclude: [/node_modules/],
-        use: [
-          {
-            loader: '@kaze-style/webpack-loader',
-            options: {},
-          },
-        ],
-      }
-    );
-
-    const cssRules = config.module.rules.find(
-      (rule) =>
-        Array.isArray(rule.oneOf) &&
-        rule.oneOf.some(
-          ({ test }) =>
-            typeof test === 'object' &&
-            typeof test.test === 'function' &&
-            test.test('filename.css'),
-        ),
-    ).oneOf;
-
-    cssRules.unshift({
-      test: /kaze\.css$/i,
-      sideEffects: true,
-      use: getGlobalCssLoader(
-        {
-          assetPrefix: config.assetPrefix,
-          isClient: !isServer,
-          isServer,
-          isDevelopment: dev,
-          future: {},
-          experimental: {},
-        },
-        () => lazyPostCSS(dir, getSupportedBrowsers(dir, dev),undefined),
-        [],
-      ),
-    });
-
-    config.plugins.push(new KazePlugin());
-
-    return config;
-  },
 };
 
-export default nextConfig;
+export default withKazeStyle(nextConfig);
