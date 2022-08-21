362b692a1c5d1d653b414f1873288f0cc10bc14e
taishinaritomi <taishinaritomi@gmail.com>
2022-08-21 15:13:30 +0900

    add reset.css

--- playground/next/src/pages/_app.tsx
+++ playground/next/src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import type { AppProps } from 'next/app';
-// import '@kaze-style/react/reset.css';
+import '@kaze-style/react/reset.css';
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
