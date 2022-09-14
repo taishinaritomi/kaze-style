84d809ecebeeb7166933221d8f0c80a475547147
taishinaritomi <taishinaritomi@gmail.com>
2022-09-14 14:06:04 +0900

    Change in build (tsdx build to esbuild)

---
+++ scripts/build.ts
@@ -0,0 +1,36 @@
+import { exec } from 'child_process';
+import { build } from 'esbuild';
+import { nodeExternalsPlugin } from 'esbuild-node-externals';
+import glob from 'fast-glob';
+
+console.log(process.argv[2] === 'watch');
+const isWatch = process.argv[2] === 'watch';
+
+Promise.all([
+  build({
+    watch: isWatch,
+    entryPoints: glob.sync('./src/**/*.ts', { ignore: ['**/*.spec.ts'] }),
+    logLevel: 'info',
+    format: 'esm',
+    minify: true,
+    outdir: 'dist',
+  }),
+  build({
+    watch: isWatch,
+    entryPoints: ['./src'],
+    logLevel: 'info',
+    bundle: true,
+    platform: 'node',
+    format: 'cjs',
+    minify: true,
+    sourcemap: true,
+    outfile: './dist/index.cjs',
+    plugins: [nodeExternalsPlugin()],
+  }),
+]);
+
+exec(
+  `tsc ${
+    isWatch ? '-w' : ''
+  } --declaration --emitDeclarationOnly --outDir dist`,
+);
