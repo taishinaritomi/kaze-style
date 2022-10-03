f8b29294ad0e8f46d97b58ba73b0e85b33109f37
taishinaritomi <taishinaritomi@gmail.com>
2022-10-03 18:01:51 +0900

    fix react package css runtime

--- packages/react/src/createStyle.ts
+++ packages/react/src/createStyle.ts
@@ -7,33 +7,39 @@ export const createStyle = <Key extends string>(
   const { classes, cssRules } = createStyleCore(stylesByKey);
 
   if (typeof document !== 'undefined') {
-    let style = document.getElementById('kaze-style') as HTMLStyleElement;
-    if (!style) {
-      style = document.createElement('style');
+    let div = document.getElementById('kaze') as HTMLDivElement;
+    if (!div) {
+      div = document.createElement('div');
+      div.id = 'kaze';
+      const style = document.createElement('style');
+      const styleMedia = document.createElement('style');
+      const styleGlobal = document.createElement('style');
       style.id = 'kaze-style';
-      document.head.appendChild(style);
-    }
-
-    let styleMedia = document.getElementById(
-      'kaze-style-media',
-    ) as HTMLStyleElement;
-    if (!styleMedia) {
-      styleMedia = document.createElement('style');
       styleMedia.id = 'kaze-style-media';
-      document.head.appendChild(styleMedia);
+      styleGlobal.id = 'kaze-global-style';
+      div.appendChild(styleGlobal);
+      div.appendChild(style);
+      div.appendChild(styleMedia);
+      document.head.appendChild(div);
     }
 
-    const styleSheet = style.sheet;
-    const styleSheetMedia = styleMedia.sheet;
+    const style = document.getElementById('kaze-style') as HTMLStyleElement & {
+      arr: string[];
+    };
+    const styleGlobal = document.getElementById(
+      'kaze-global-style',
+    ) as HTMLStyleElement & { arr: string[] };
 
     cssRules.forEach((css) => {
       if (css.match(/@media/)) {
-        styleSheetMedia?.insertRule(css);
+        styleGlobal.arr = [...(styleGlobal.arr || []), css];
       } else {
-        styleSheet?.insertRule(css);
+        style.arr = [...(style.arr || []), css];
       }
     });
-  }
 
+    style.innerText = Array.from(new Set(style.arr)).join('');
+    styleGlobal.innerText = Array.from(new Set(styleGlobal.arr)).join('');
+  }
   return classes;
 };
