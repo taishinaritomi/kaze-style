e286a0fe7e933b0a2287078a07a7acb9d8f7b48a
taishinaritomi <taishinaritomi@gmail.com>
2022-10-04 17:42:28 +0900

    add packages build-man

---
+++ packages/build-man/src/constants.ts
@@ -0,0 +1,17 @@
+export const styleBucketOrdering = [
+  'global',
+  'normal',
+  'link',
+  'visited',
+  'focus-within',
+  'focus',
+  'focus-visible',
+  'hover',
+  'active',
+  'keyframes',
+  'at-rules',
+  'media',
+] as const;
+
+export const NORMAL_STYLE_LAYER = 'kazeStyle';
+export const GLOBAL_STYLE_LAYER = 'kazeGlobalStyle';
