bc3de7b771532f2d8af78722c4c2d562939f091e
taishinaritomi <taishinaritomi@gmail.com>
2022-11-03 20:40:38 +0900

    fix test

--- packages/core/src/createGlobalStyle.spec.ts
+++ packages/core/src/createGlobalStyle.spec.ts
@@ -26,4 +26,52 @@ describe('createStyle', () => {
       { order: 'global', rule: 'html{color:red;}' },
     ]);
   });
+
+  it('font-face', () => {
+    const { cssRuleObjects } = createGlobalStyle({
+      '@font-face': {
+        fontFamily: 'Open Sans',
+        src: 'url("/fonts/OpenSans-Regular-webfont.woff2") format("woff2")',
+      },
+    });
+    expect(cssRuleObjects).toEqual([
+      {
+        order: 'global',
+        rule: '@font-face{font-family:Open Sans;src:url("/fonts/OpenSans-Regular-webfont.woff2") format("woff2");}',
+      },
+    ]);
+  });
+
+  it('mix', () => {
+    const { cssRuleObjects } = createGlobalStyle({
+      '@font-face': {
+        fontFamily: 'Open Sans',
+        src: 'url("/fonts/OpenSans-Regular-webfont.woff2") format("woff2")',
+      },
+      html: {
+        color: 'red',
+        '@media (max-width:1024px)': {
+          color: 'green',
+        },
+        '@media (max-width:512px)': {
+          color: 'blue',
+        },
+      },
+    });
+    expect(cssRuleObjects).toEqual([
+      {
+        order: 'global',
+        rule: '@font-face{font-family:Open Sans;src:url("/fonts/OpenSans-Regular-webfont.woff2") format("woff2");}',
+      },
+      {
+        order: 'global',
+        rule: '@media (max-width:1024px){html{color:green;}}',
+      },
+      {
+        order: 'global',
+        rule: '@media (max-width:512px){html{color:blue;}}',
+      },
+      { order: 'global', rule: 'html{color:red;}' },
+    ]);
+  });
 });
