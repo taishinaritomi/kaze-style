b1ff9b4632cc2bf0cbc1fb9acc851958b5074d2d
taishinaritomi <taishinaritomi@gmail.com>
2022-11-29 13:53:18 +0900

    update dependency version

--- playground/react/webpack.config.js
+++ playground/react/webpack.config.js
@@ -25,10 +25,7 @@ module.exports = {
               presets: [
                 '@babel/preset-typescript',
                 ['@babel/preset-react', { runtime: 'automatic' }],
-                [
-                  '@babel/preset-env',
-                  { targets: { node: 14 }, modules: false },
-                ],
+                ['@babel/preset-env', { targets: { browsers: 'defaults' } }],
               ],
             },
           },
