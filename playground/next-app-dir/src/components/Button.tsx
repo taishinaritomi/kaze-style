94a701765beeee01c8c3bbb10efcd5778390add0
Taishi Naritomi <79634025+taishinaritomi@users.noreply.github.com>
2022-11-29 13:22:26 +0900

    Support next app dir without Server Component (#69)
    * update next version 13
    
    * add next app dir playground
    
    * update version 0.11.2
    
    * fix error
    
    * fix code format

---
+++ playground/next-app-dir/src/components/Button.tsx
@@ -0,0 +1,21 @@
+import { createStyle, mergeStyle } from '@kaze-style/react';
+import type { ComponentProps, FC } from 'react';
+
+const classes = createStyle({
+  button: {
+    background: 'red',
+    borderRadius: '6px',
+    ':hover': {
+      background: 'orange',
+    },
+  },
+});
+
+export const Button: FC<ComponentProps<'button'>> = (props) => {
+  return (
+    <button
+      {...props}
+      className={mergeStyle(classes.button, props.className)}
+    />
+  );
+};
