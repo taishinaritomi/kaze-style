a8858bf637cbd068ab2f8004d61fef7c3fb1ae2c
taishinaritomi <taishinaritomi@gmail.com>
2023-02-25 21:22:24 +0900

    fix webpack-plugin

--- packages/webpack-plugin/src/constatns.ts
+++ packages/webpack-plugin/src/constatns.ts
@@ -1,6 +1,3 @@
 export const DUMMY_CSS_FILE_PATH = require.resolve(
-  '@kaze-style/webpack-plugin/assets/kaze.css',
-);
-export const DUMMY_JS_FILE_PATH = require.resolve(
-  '@kaze-style/webpack-plugin/assets/kaze.js',
+  '@kaze-style/webpack-plugin/assets/DUMMY.css',
 );
