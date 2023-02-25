71c0c438acb3be89e853ece550905cc0a241ee23
taishinaritomi <taishinaritomi@gmail.com>
2023-02-25 20:53:28 +0900

    fix playground

--- playground/next/src/pages/index.tsx
+++ playground/next/src/pages/index.tsx
@@ -1,27 +1,11 @@
-import { mergeStyle, style } from '@kaze-style/core';
+import { mergeStyle } from '@kaze-style/core';
 import type { NextPage } from 'next';
 import dynamic from 'next/dynamic';
 import { Button } from '@/components/Button';
-// import { classes } from '@/styles/index.style';
+import { classes } from '@/styles/index.style';
 const LazyButton = dynamic(() =>
   import('@/components/Button').then((module) => module.Button),
 );
-
-const classes = style({
-  container: {
-    display: 'flex',
-    gap: '5px',
-  },
-  $button: {
-    padding: '10px 20px',
-  },
-  $blueButton: {
-    background: 'blue',
-    ':hover': {
-      background: 'skyblue',
-    },
-  },
-});
 
 const Home: NextPage = () => {
   return (
