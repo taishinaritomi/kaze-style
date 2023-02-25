110c271725585c5d8b3382cea672ea6a51f4f558
taishinaritomi <taishinaritomi@gmail.com>
2023-02-25 21:21:58 +0900

    fix playground

---
+++ playground/react/src/App.style.ts
@@ -0,0 +1,17 @@
+// import { classes } from './App.style';
+
+import { globalStyle, style } from '@kaze-style/core';
+import { globalTheme } from '@kaze-style/themes';
+
+globalStyle(globalTheme.reset());
+
+export const classes = style({
+  button: {
+    background: 'red',
+    borderRadius: '6px',
+    padding: '10px 20px',
+    ':hover': {
+      background: 'orange',
+    },
+  },
+});
--- playground/react/src/App.tsx
+++ playground/react/src/App.tsx
@@ -1,20 +1,4 @@
-// import { classes } from './App.style';
-
-import { globalStyle, style } from '@kaze-style/core';
-import { globalTheme } from '@kaze-style/themes';
-
-globalStyle(globalTheme.reset());
-
-export const classes = style({
-  button: {
-    background: 'red',
-    borderRadius: '6px',
-    padding: '10px 20px',
-    ':hover': {
-      background: 'orange',
-    },
-  },
-});
+import { classes } from './App.style';
 
 export const App = () => {
   return <button className={classes.button}>button</button>;
