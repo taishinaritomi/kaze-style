98c3f9c5eaee63a0e550cd3ac42d6fd9c75feefa
taishinaritomi <taishinaritomi@gmail.com>
2023-03-23 21:20:56 +0900

    update playground

--- playground/react-vite/vite.config.ts
+++ playground/react-vite/vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [react(), kazePlugin(/* { cssLayer: true, swc: true } */)],
+  plugins: [react(), kazePlugin({ cssLayer: true, swc: true })],
 });
