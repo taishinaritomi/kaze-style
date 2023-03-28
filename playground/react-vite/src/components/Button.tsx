9921723ac4b9d43f3253e151d3d0ddaac2eaea57
taishinaritomi <taishinaritomi@gmail.com>
2023-03-28 13:41:56 +0900

    $className to className

--- playground/react-vite/src/components/Button.tsx
+++ playground/react-vite/src/components/Button.tsx
@@ -9,9 +9,7 @@ export const Button: FC<ClassNameOverride<ComponentProps<'button'>>> = (
   return (
     <button
       {...props}
-      className={`${mergeStyle(classes.$button, props.$className)} ${
-        props.className || ''
-      }`}
+      className={`${mergeStyle(classes.$button, props.className)}`}
     />
   );
 };
